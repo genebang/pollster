dcc6144e7087a4f0b99e475b36b62560652c61d1
Gene Bang <genebang@gmail.com>
2012-11-20 16:43:33 -0800

    working fadeout button on taker form

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -9,9 +9,10 @@ class Poll < ActiveRecord::Base
   private
 
   def set_admin_and_public_url
-    url = (('a'..'z').to_a + ('A'..'Z').to_a + (1..9).to_a).shuffle[0..5].join
-    self.admin_url = url
-    self.public_url = url
+    url_a = (('a'..'z').to_a + ('A'..'Z').to_a + (1..9).to_a).shuffle[0..5].join
+    url_p = (('a'..'z').to_a + ('A'..'Z').to_a + (1..9).to_a).shuffle[0..5].join
+    self.admin_url = url_a
+    self.public_url = url_p
   end
 
 end
