31b06441127e50f4250fc2e7bc215dcddccad956
Gene Bang <genebang@gmail.com>
2012-11-22 00:07:15 -0800

    working mp answers for taker, formatting needed

--- app/assets/javascripts/taker.js
+++ app/assets/javascripts/taker.js
@@ -1,26 +1,28 @@
 $(document).ready( function() {
   
+  // set mp form on page load
   if ($('select').val() === "multiple_choice" || $('select').val() === "checkboxes") {
     $('div.choices').show();
   }
-    // hide response when taker answers question
-    $('.btn').click(function() {
-        $(this).parent().parent('.taker_response').fadeOut();
-    });
-    
-    //  show and hide mp options for question
-    $('select').change(function() {
-        // $(this).parent().parent('.question_form').fadeOut();
-        var value = $(this).val();
-        if (value === "text") {
-          $('div.choices').hide();
-        }
-        if (value === "multiple_choice") {
-          $('div.choices').show();
-        }
-        if (value === "checkboxes") {
-          $('div.choices').show();
-        }
-    });
+  
+  // hide response when taker answers question
+  $('.btn').click(function() {
+      $(this).parent().parent('.taker_response').fadeOut();
+  });
+  
+  //  show and hide mp options for question
+  $('select').change(function() {
+      // $(this).parent().parent('.question_form').fadeOut();
+      var value = $(this).val();
+      if (value === "text") {
+        $('div.choices').hide();
+      }
+      if (value === "multiple_choice") {
+        $('div.choices').show();
+      }
+      if (value === "checkboxes") {
+        $('div.choices').show();
+      }
+  });
 
 });
